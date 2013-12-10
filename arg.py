6ba8fe4640c35a3a14a93c224f625cfbe49a00ab
fabs <fyamagu@gwdg.de>
2013-12-10 02:02:28 +0100

    Implemented '--symbols' feature for arg.py to extract symbols used instead of the code of an argument. Doing this on arguments is just an example, really.

--- arg.py
+++ arg.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python2
 
-import sys
+import sys, argparse
 from joern.all import JoernSteps
 
-def usage():
-    print ('%s <callee> <argument>' % (sys.argv[0]))
+class CLI():
+    def __init__(self):
+        self._initializeOptParser()
+        self._parseCommandLine()
+    
+    def _initializeOptParser(self):
+        self.argParser = argparse.ArgumentParser(description =\
+        "Returns all n'th arguments of calls to the specified\
+        callee.") 
+        self.argParser.add_argument('callee', type=str)
+        self.argParser.add_argument('n', type=str)
+        self.argParser.add_argument('-s', '--symbols',
+        action='store_true', help='return symbols used rather than\
+        code.')
+   
+    def _parseCommandLine(self):
+        self.args = self.argParser.parse_args()
+
+    def run(self):
+        
+        j = JoernSteps()
+        j.connectToDatabase()
+
+        query = """
+        getArgumentNTo("%s", "%d")
+        .sideEffect{ code = it.code; node = it; }
+        .astNodeToBasicBlock()
+        .sideEffect{loc = it.location;}.transform{ node }
+        """ % (sys.argv[1], int(sys.argv[2]) - 1)
+
+        if self.args.symbols:
+            query += """.astNodeToSymbolsUsed().sideEffect{sym = it.code;
+            }.transform{ node } """
+        
+        query += """
+        .functionAndFilename().sideEffect{ (funcName, filename) = it }
+        .transform{ [code, funcName, filename, loc] }
+        """ 
+    
+        y = j.runGremlinQuery(query)
+        for x in y:
+            print '%s\t%s\t%s\t%s' % tuple(x)
 
 if __name__ == '__main__':
     
-    j = JoernSteps()
-    j.connectToDatabase()
+    cli = CLI()
+    cli.run()
 
-    if len(sys.argv) != 3:
-        usage()
-        sys.exit()
-
-    query = """
-    getArgumentNTo("%s", "%d")
-    .sideEffect{ code = it.code;}
-    .astNodeToBasicBlock()
-    .sideEffect{loc = it.location; }
-    .back(2)
-    .astNodeToFunction
-    .sideEffect{funcName = it.functionName;}
-    .functionToFile
-    .sideEffect{ filename = it.filepath; }
-    .transform{ [code, funcName, filename, loc] }
-
-""" % (sys.argv[1], int(sys.argv[2]) - 1)
-    
-    y = j.runGremlinQuery(query)
-    for x in y:
-        print '%s\t%s\t%s\t%s' % tuple(x)
