5a5b421fac09d2d2a9740b1ff54cd63edee03183
fabs <fyamagu@gwdg.de>
2013-12-14 12:27:32 +0100

    Added depth of node to output of subTree.py.

--- subTree.py
+++ subTree.py
@@ -21,10 +21,11 @@ class SubTree(PipeTool):
         nodeId = int(line)
         
         query = """ g.v(%d).astNodeToSubNodes()
-        .transform{ [it.id, it.type, it.code] }""" % (nodeId)
+        .transform{ [it[0].id, it[1], it[0].type, it[0].code] }""" % (nodeId)
+
         y = self.j.runGremlinQuery(query)
         for x in y:
-            print '%s\t%s\t%s' % tuple(x)
+            print '%s\t%s\t%s\t%s' % tuple(x)
 
 if __name__ == '__main__':
     tool = SubTree()
