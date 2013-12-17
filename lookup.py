51d0a28b2a78f824b4d730024b2e056c076c817b
fabs <fyamagu@gwdg.de>
2013-12-17 18:03:53 +0100

    lookup.py is now more generic, allowing arbitrary lucene queries to be specified.

--- lookup.py
+++ lookup.py
@@ -4,12 +4,15 @@ from ParseLocationString import parseLocationOrFail
 from PipeTool import PipeTool
 from joern.all import JoernSteps
 
-DESCRIPTION = """Lookup AST nodes by type and name"""
+DESCRIPTION = """Lookup nodes using lucene queries and output one row
+for each property each matching node. Rows are in the format '[nodeId,
+attributeName, attributeValue]'.
+"""
 
 class Lookup(PipeTool):
     def __init__(self):
         PipeTool.__init__(self, DESCRIPTION)
-    
+
     # @Override
     def streamStart(self):
         self.j = JoernSteps()
@@ -17,18 +20,23 @@ class Lookup(PipeTool):
 
     # @Override
     def processLine(self, line):
-        (nodeType, code) = line.split('\t')
+        query = line
+        if query.startswith('id:'):
+            id = query.split(':')[1]
+            query = 'g.v(%s).transform{ [it.id, it] }' % (id)
+        else:
+            query = """queryNodeIndex('%s').transform{ [it.id, it]}""" % (query)
         
-        query = """queryNodeIndex('type:%s AND code:"%s"')""" % (nodeType, code)
-        query += """
-        .astNodeToFunction().sideEffect{ loc = it.location; name = it.functionName}
-        .functionToFile().sideEffect{filename = it.filepath}
-        .transform{[name, filename, loc]}
-        """
-
         y = self.j.runGremlinQuery(query)
         for x in y:
-            self.output('%s\t%s:%s\n' % tuple(x))
+            self._outputRecord(x)
+            
+    def _outputRecord(self, record):
+        id, node = record
+        keys = [k for k in node]
+        keys.sort()
+        keyValPairs = [str(k) + ':' + str(node[k]) for k in keys]
+        self.output('id:%s\t%s\n' % (id, '\t'.join(keyValPairs)) )
 
 if __name__ == '__main__':
     tool = Lookup()
