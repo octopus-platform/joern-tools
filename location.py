541b0374c8b01e7935b7ba366d71bf12c4601710
fabs <fyamagu@gwdg.de>
2013-12-19 00:02:34 +0100

    Location tool to get the location of code by node id.

---
+++ location.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+
+import sys
+from LookupTool import LookupTool
+
+DESCRIPTION = """Lookup the location of a node in the code."""
+
+class Location(LookupTool):
+    
+    def __init__(self):
+        LookupTool.__init__(self, DESCRIPTION)
+        
+     # @Override
+    def processLine(self, line):
+ 
+        # For functions, get location of function
+        # For statement, get location of statement
+        # For AST nodes, get location of statement
+        # For Symbols, get location of function
+        
+        id = line
+        query = """g.v(%s)
+        .ifThenElse{it.type == 'Function'}{
+         it.sideEffect{loc = it.location; }.functionToFile()
+         .sideEffect{filename = it.filepath; }
+         }{
+           it.ifThenElse{it.type == 'Symbol'}
+           {
+             it.transform{ g.v(it.functionId) }.sideEffect{loc = it.location; }
+             .functionToFile()
+             .sideEffect{filename = it.filepath; }
+            }{
+             it.ifThenElse{it.type == 'BasicBlock'}{
+               it.sideEffect{loc = it.location}.basicBlockToAST()
+               .astNodeToFunction().functionToFile()
+               .sideEffect{filename = it.filepath; }
+             }{
+              // AST node
+              it.astNodeToBasicBlock().sideEffect{loc = it.location; }
+              .basicBlockToAST().astNodeToFunction()
+              .functionToFile().sideEffect{filename = it.filepath; }
+              }
+           }
+        }.transform{ filename + ':' + loc }
+        
+        """ % (id)
+
+        y = self.j.runGremlinQuery(query)
+        for x in y:
+            print x
+        
+
+if __name__ == '__main__':
+    tool= Location()
+    tool.run()
