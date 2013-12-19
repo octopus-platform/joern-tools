7e9c203b1cee7c3882539fa354d99977e9e01412
fabs <fyamagu@gwdg.de>
2013-12-19 16:32:47 +0100

    Removed pickle-output from subTree.py as its not useful and broken.

--- csvAST/CSVToPythonAST.py
+++ csvAST/CSVToPythonAST.py
@@ -43,20 +43,9 @@ class CSVToPythonAST(CSVProcessor):
             self.rootNode = self.rootNode.children[0]
         return self.rootNode
     
-def pythonASTFromDbResult(dbResult):
-    csvRows = (_csvRow(z) for z in dbResult)
-    return pythonASTFromCSV(csvRows)
-
 def pythonASTFromCSV(csvRows):
     converter = CSVToPythonAST()
     converter.processCSVRows(csvRows)
     return converter.getResult()
 
-def _csvRow(z):
-    nodeId = z[0]
-    x = z[1]
-    if x[0]['operator'] == None:
-        return '%s\t%s\t%s\t%s' % (nodeId, x[1], x[0]['type'], x[0]['code']) 
-    else:
-        return '%s\t%s\t%s\t%s' % (nodeId, x[1], x[0]['type'], x[0]['operator'])
-        
+    
--- subTree.py
+++ subTree.py
@@ -3,12 +3,8 @@
 from joern.all import JoernSteps
 from PipeTool import PipeTool
 
-from csvAST.CSVToPythonAST import pythonASTFromDbResult
-import pickle
-
 DESCRIPTION = """Prints all nodes of the AST rooted at the node with
-the given id. The default output format is a CSV format similar to
-that used by CodeSensor."""
+the given id. Output format is rootNodeId TAB nodeId TAB level."""
 
 OUTPUT_CSV = 'csv'
 OUTPUT_PICKLE= 'pickle'
@@ -18,31 +14,14 @@ class SubTree(PipeTool):
     
     def __init__(self):
         PipeTool.__init__(self, DESCRIPTION)
-
-        self.argParser.add_argument('-O', '--output-format', choices = OUTPUT_FORMATS,
-                                    action='store', help="""The output
-                                    format to use""",
-                                    default = OUTPUT_CSV)
-    
-        self._initOutputHandlers()
-
-    def _initOutputHandlers(self):
-        self.outputHandler = dict()
-        self.outputHandler[OUTPUT_CSV] = self._outputCSV
-        self.outputHandler[OUTPUT_PICKLE] = self._outputPickle
     
     def _outputCSV(self, dbResult, rootNodeId):
         for z in dbResult:
             id = z[0]
             x = z[1]
-            # rootNodeId, nodeId, depth, type, code
-            code = x[0]['code'].replace('\t', ' ')
-            self.output('%s\t%s\t%s\t%s\t%s\n' % (rootNodeId, id, x[1], x[0]['type'], x[0]['code']))
-            
-    def _outputPickle(self, dbResult, nodeId):
-        pythonAST = pythonASTFromDbResult(dbResult)
-        pickle.dump(pythonAST, self.args.out, protocol=2)
-        
+            # rootNodeId nodeId depth
+            self.output('%s\t%s\t%s\n' % (rootNodeId, id, x[1]))
+    
     # @Override
     def streamStart(self):
         self.j = JoernSteps()
@@ -53,10 +32,8 @@ class SubTree(PipeTool):
         nodeId = int(line)
         
         query = """g.v(%d).astNodeToSubNodes().transform{ [it[0].id, it] }""" % (nodeId)
-
         dbResult = self.j.runGremlinQuery(query)
-        
-        self.outputHandler[self.args.output_format](dbResult, nodeId)
+        self._outputCSV(dbResult, nodeId)
     
     
 if __name__ == '__main__':
