8e6eac6037506b23de6e3f3803189665408dcc10
fabs <fyamagu@gwdg.de>
2013-12-19 11:39:33 +0100

    Fixed examples.

--- examples/apiEmbed.sh
+++ examples/apiEmbed.sh
@@ -2,7 +2,7 @@
 
 # Represent all functions using 'memcpy' by its Callees and the types of parameters and local variables
 
-echo 'type:Symbol AND code:memcpy' | ./lookup.py | ./function.py | sort | uniq | awk '{print "functionId:" $1}' | ./lookup.py --attributes type functionId code | egrep 'type:(Callee|ParameterType|IdentifierDeclType)' | awk '{split($3, a, ":"); print a[2] "\t" $4}' | ./demux.py --outputDir bagOfWords
+echo 'type:Symbol AND code:memcpy' | ./lookup.py --attributes functionId | awk '{split($2,a,":"); print "functionId:" a[2]}' | ./lookup.py --attributes type functionId code | egrep 'type:(Callee|ParameterType|IdentifierDeclType)' | awk '{split($3, a, ":"); split($4,b,":"); print a[2] "\t" b[2]}' | ./demux.py --outputDir bagOfWords
 
 sally -n 1 --input_format=dir bagOfWords/data/ bagOfWords/embedding.libsvm --hash_file bagOfWords/feats.gz --vect_embed=tfidf
 
