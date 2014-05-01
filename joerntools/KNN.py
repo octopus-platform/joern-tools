9fb6f15133fedf4e60b334caeccd011d36a6ac50
fabs <fyamagu@gwdg.de>
2014-05-01 19:33:36 +0200

    Allow KNN to be used programmatically and not just via the shell.

---
+++ joerntools/KNN.py
@@ -0,0 +1,78 @@
+
+from joerntools.mlutils.EmbeddingLoader import EmbeddingLoader
+from joerntools.mlutils.EmbeddingSaver import EmbeddingSaver
+from sklearn.metrics.pairwise import pairwise_distances
+
+
+class KNN():
+    def __init__(self):
+        self.loader = EmbeddingLoader()
+        self.saver = EmbeddingSaver() 
+
+    
+    def setEmbeddingDir(self, dirname):
+        self.dirname = dirname
+    
+    def setLimitArray(self, limit):
+        self.limit = limit
+    
+    def setK(self, k):
+        self.k = k
+    
+    def setNoCache(self, no_cache):
+        self.no_cache = no_cache
+    
+    def initialize(self):
+        
+        self.emb = self._loadEmbedding(self.dirname)
+        if self.limit:
+            self._loadValidNeighbors()
+
+
+    def _loadEmbedding(self, dirname):
+        self.saver.setEmbeddingDir(dirname)
+        return self.loader.load(dirname, tfidf=False, svd_k=0)
+        
+
+    def _loadValidNeighbors(self):
+        self.validNeighbors = [int(x) for x in
+                                   self.limit.readlines()]
+
+    
+    def getNeighborsFor(self, line):
+        self.calculateDistances()
+        
+        dataPointIndex = self.emb.rTOC[line]    
+        nReturned = 0
+            
+        for i in self.emb.NNI[:, dataPointIndex]:
+            
+            if self.limit:
+                if not int(self.emb.TOC[i]) in self.validNeighbors:
+                    continue
+
+            print self.emb.TOC[i]
+            nReturned += 1
+            
+            if nReturned == self.k:
+                break
+    
+    def calculateDistances(self):
+        if not self.emb.dExists():
+            self.emb.D = self._calculateDistanceMatrix()
+     
+            if not self.no_cache:
+                self.saver.saveDistanceMatrix(self.emb)
+            
+        if not self.emb.nnExists():
+            self._calculateNearestNeighbors()
+            if not self.no_cache:
+                self.saver.saveNearestNeighbors(self.emb)
+            
+    def _calculateNearestNeighbors(self):
+        self.emb.NNV = self.emb.D.copy()
+        self.emb.NNI = self.emb.D.argsort(axis=0)
+        self.emb.NNV.sort(axis=0)
+        
+    def _calculateDistanceMatrix(self):
+        return pairwise_distances(self.emb.x, metric='cosine')
--- knn.py
+++ knn.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python2
 
 from joerntools.shelltool.PipeTool import PipeTool
-from joerntools.mlutils.EmbeddingLoader import EmbeddingLoader
-from joerntools.mlutils.EmbeddingSaver import EmbeddingSaver
-
-from sklearn.metrics.pairwise import pairwise_distances
 from argparse import FileType
 
 import sys
-
-DESCRIPTION = """ Calculate the k nearest neighbors to a data point
-based on an embedding. """
+from joerntools.KNN import KNN
 
 DEFAULT_DIRNAME = 'embedding'
 DEFAULT_K = 10
 
-class KNN(PipeTool):
+class KNNCLI(PipeTool):
     
-    def __init__(self):
-        PipeTool.__init__(self, DESCRIPTION)
-        self.loader = EmbeddingLoader()
-        self.saver = EmbeddingSaver()
-
+    def __init__(self, description):
+        PipeTool.__init__(self, description)
+       
         self.argParser.add_argument('-k', '--k', nargs='?', type=int,
                                     help =""" number of nearest
                                     neighbors to determine""",
@@ -40,72 +32,35 @@ class KNN(PipeTool):
                                     help = """ Limit possible
                                     neighbours to those specified in
                                     the provided file.""")
-        
 
-    def _loadEmbedding(self, dirname):
-        self.saver.setEmbeddingDir(dirname)
-        try:
-            return self.loader.load(dirname, tfidf=False, svd_k=0)
-        except IOError:
-            sys.stderr.write('Error reading embedding.\n')
-            sys.exit()
+        self.knn = KNN()
+        
 
     # @Override
     def streamStart(self):
-        self.emb = self._loadEmbedding(self.args.dirname)
-        if self.args.limit:
-            self._loadValidNeighbors()
-
-    def _loadValidNeighbors(self):
-        self.validNeighbors = [int(x) for x in
-                                   self.args.limit.readlines()]
-
-
-    # @Override
-    def processLine(self, line):
-        self.calculateDistances()
+        
+        self.knn.setEmbeddingDir(self.args.dirname)
+        self.knn.setLimitArray(self.args.limit)
+        self.knn.setK(self.args.k)
+        self.knn.setNoCache(self.args.no_cache)
         
         try:
-            dataPointIndex = self.emb.rTOC[line]
+            self.knn.initialize()
+        except IOError:
+            sys.stderr.write('Error reading embedding.\n')
+            sys.exit()
+    
+    # @Override
+    def processLine(self, line):
+        
+        try:
+            self.knn.getNeighborsFor(line)
         except KeyError:
             sys.stderr.write('Warning: no data point found for %s\n' %
                              (line))
-            
-        nReturned = 0
-            
-        for i in self.emb.NNI[:, dataPointIndex]:
-            
-            if self.args.limit:
-                if not int(self.emb.TOC[i]) in self.validNeighbors:
-                    continue
-
-            print self.emb.TOC[i]
-            nReturned += 1
-            
-            if nReturned == self.args.k:
-                break
-
-    def calculateDistances(self):
-        if not self.emb.dExists():
-            self.emb.D = self._calculateDistanceMatrix()
-     
-            if not self.args.no_cache:
-                self.saver.saveDistanceMatrix(self.emb)
-            
-        if not self.emb.nnExists():
-            self._calculateNearestNeighbors()
-            if not self.args.no_cache:
-                self.saver.saveNearestNeighbors(self.emb)
-            
-    def _calculateNearestNeighbors(self):
-        self.emb.NNV = self.emb.D.copy()
-        self.emb.NNI = self.emb.D.argsort(axis=0)
-        self.emb.NNV.sort(axis=0)
-        
-    def _calculateDistanceMatrix(self):
-        return pairwise_distances(self.emb.x, metric='cosine')
 
 
 if __name__ == '__main__':
-    tool = KNN()
+    description = """ Calculate the k nearest neighbors to a data point based on an embedding. """
+    tool = KNNCLI(description)
     tool.run()
