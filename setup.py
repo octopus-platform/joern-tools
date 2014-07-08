66ce4dd12d9b7b43a26b738f71d4c417060f3fb3
fabs <fyamagu@gwdg.de>
2014-07-08 11:42:22 +0200

    Cleanup of joern-tools.

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ import os, fnmatch
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+def getScripts():
+    x = []
+    for (dirpath, dirnames, filenames) in os.walk('tools'):
+        x.extend(os.path.join(dirpath,f) for f in filenames if f.endswith('.py'))
+    return x
+
 setup(
     name = "joerntools",
     version = "0.1",
@@ -17,7 +23,7 @@ setup(
     long_description = read('README.md'),
     packages = find_packages(),
     package_data={"joerntools": ['steps/*',]},
-    scripts = [f for f in os.listdir('.') if fnmatch.fnmatch(f, '*.py')],
+    scripts = getScripts(),
     install_requires = ['joern >= 0.1', 'pygraphviz'],
     dependency_links = ['https://github.com/fabsx00/python-joern/tarball/master/#egg=joern-0.1'],
     zip_safe = False
--- traversal.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python2
-
-from joerntools.shelltool.TraversalTool import TraversalTool
-
-DESCRIPTION = """ Run an arbitrary gremlin traversal. """
-
-if __name__ == '__main__':
-    tool = TraversalTool(DESCRIPTION)
-    tool.run()
