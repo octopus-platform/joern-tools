781dced9f1ceee6a397c5d7b2f7a971caedbeeb2
fabs <fyamagu@gwdg.de>
2014-07-22 00:04:37 +0200

    joern-edge: For a set of nodes, returns all edges connecting nodes of the set.

--- joerntools/steps/steps.groovy
+++ joerntools/steps/steps.groovy
@@ -5,3 +5,12 @@ Gremlin.defineStep('functionToAPISymbolNodes', [Vertex, Pipe], {
         .filter{it.type == 'IdentifierDeclType' || it.type == 'ParameterType' || it.type == 'Callee' || it.type == 'Sizeof'}
 });
 
+
+Object.metaClass.edgesInX = { X -> 
+	X._().transform{ g.v(it) }
+	.outE()
+	.filter{
+		p = it.inV().id.toList()[0]
+		X.contains(p)
+	}.id
+}
