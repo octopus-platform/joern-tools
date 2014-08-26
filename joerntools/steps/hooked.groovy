05e099b3ba9aa8d461a7b293022415d1db30e976
fabs <fyamagu@gwdg.de>
2014-08-26 16:12:41 +0200

    Make more lookup-functions 'visibility'-capable.

--- joerntools/steps/hooked.groovy
+++ joerntools/steps/hooked.groovy
@@ -10,3 +10,28 @@ Object.metaClass.queryNodeIndex = { query, honorVisibility = true ->
 		new Neo4jVertexSequence(index.query(query), g)._()
 }
 
+Object.metaClass.getFilesByName = { filename, honorVisibility = true ->
+	query = "$NODE_TYPE:$TYPE_FILE AND $NODE_FILEPATH:$filename"
+	queryNodeIndex(query, honorVisibility)
+}
+
+Object.metaClass.getFunctionsByName = { name, honorVisibility = true ->
+	getNodesWithTypeAndName(TYPE_FUNCTION, name, honorVisibility)
+}
+
+Object.metaClass.getNodesWithTypeAndName = { type, name, honorVisibility = true  ->
+	query = "$NODE_TYPE:$type AND $NODE_NAME:$name"
+	queryNodeIndex(query, honorVisibility)
+}
+
+Object.metaClass.getFunctionsByFileAndName = { filename, name, honorVisibility = true ->
+	getFunctionsByFilename(filename, honorVisibility)
+	.filter{ it.name == name }
+}
+
+Object.metaClass.getFunctionsByFilename = { name, honorVisibility = true ->
+	query = "$NODE_TYPE:$TYPE_FILE AND $NODE_FILEPATH:$name"
+	queryNodeIndex(query, honorVisibility)
+	.out('IS_FILE_OF')
+	.filter{ it.type == TYPE_FUNCTION }
+}
